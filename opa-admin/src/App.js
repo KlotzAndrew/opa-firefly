9e9b5eead4eca6e0549d402b021198db2de54afc
Andrew Klotz <agc.klotz@gmail.com>
2018-09-16 22:23:53 +0000

    show policies

--- opa-admin/src/App.js
+++ opa-admin/src/App.js
@@ -1,20 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios'
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {policies: []};
+  }
+
+  componentWillMount() {
+    this.getPolicies();
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        { this.listPolicies() }
       </div>
     );
+  }
+
+  listPolicies = () => {
+    var res = []
+    this.state.policies.forEach(p => {
+      res.push(<div key={p}>
+        <div>{p}</div>
+      </div>)
+    })
+    return res
+  }
+
+  getPolicies = () => {
+    let axiosInstance = axios.create({baseURL: `http://0.0.0.0:8000`});
+
+    axiosInstance
+      .get("")
+      .then(resp => {
+        var regex = /\w+(.rego)/g;
+        var found = resp.data.match(regex);
+
+        let res = []
+        found.forEach(p => {
+          if (!p.includes("_test") && !res.includes(p)) {
+            res.push(p)
+          }
+        })
+
+        this.setState((prevState, props) => ({
+          policies: res,
+        }));
+      })
   }
 }
 
---
+++ policies/server.py
@@ -0,0 +1,9 @@
+from SimpleHTTPServer import SimpleHTTPRequestHandler, BaseHTTPServer
+
+class CORSRequestHandler (SimpleHTTPRequestHandler):
+    def end_headers (self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        SimpleHTTPRequestHandler.end_headers(self)
+
+if __name__ == '__main__':
+    BaseHTTPServer.test(CORSRequestHandler, BaseHTTPServer.HTTPServer)
